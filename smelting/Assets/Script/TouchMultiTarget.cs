04d532b124afe2792e86febdaa19ce7be757106c
Sherry23456 <1025182546@qq.com>
2026-04-23 15:14:22 +0800

    1

--- smelting/Assets/Script/TouchMultiTarget.cs
+++ smelting/Assets/Script/TouchMultiTarget.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.Video;
 public class TouchMultiTarget : MonoBehaviour
@@ -16,7 +17,11 @@ public class TouchMultiTarget : MonoBehaviour
     [Header("点击检测参数")]
     [Tooltip("允许的最大移动像素（超过则判定为滑动）")]
     public float maxMoveDistance = 20f;
-
+    [Header("预制体")]
+    public GameObject[] yuzhiti;
+    [Header("预制体父物体")]
+    public GameObject fatherobj;
+    public TMP_Text text1;
     // 记录触摸起始位置
     private Vector2 touchStartPos;
     //视频
@@ -73,12 +78,44 @@ public class TouchMultiTarget : MonoBehaviour
             }
         }
     }
-
+    /// <summary>
+    /// 预制体
+    /// </summary>
+    void YuzhiiX(int a)
+    {
+        GameObject newyuzhiti = Instantiate(yuzhiti[a]);
+        newyuzhiti.transform.SetParent(fatherobj.transform);
+        newyuzhiti.transform.position =  new Vector3(545.112976f, 146.203995f, 443);
+        newyuzhiti.transform.rotation = Quaternion.identity;
+        if (a==2)
+        {
+            newyuzhiti.transform.localScale = new Vector3(80, 80, 80);
+        }
+        else
+        {
+            newyuzhiti.transform.localScale = new Vector3(50, 50, 50);
+        }
+        
+    }
+    /// <summary>
+    /// 删除预制体
+    /// </summary>
+    void Dedtroyobj()
+    {
+        foreach (Transform item in fatherobj.transform)
+        {
+            Destroy(item.gameObject);
+        }
+    }
     void OnHitTarget(GameObject obj)
     {
         switch (obj.name)
         {
             case "tieli": //铁犁旋转
+                Dedtroyobj();
+                YuzhiiX(0);
+                text1.text="";
+                text1.text = "<b>铁耙</b>始于北魏，距今已有 1500 余年历史，是古代冶铁技术应用于农耕的经典器具。其主体铁齿经冶炼锻造而成，坚硬耐用，耕作深度不超过 15 厘米。《齐民要术》称其 “铁齿耙”，《王祯农书》载有方耙、人字耙等形制。铁耙结构简约实用，既体现古人冶金工艺水平，也承载着传统农耕智慧。";
                 uiimage[0].SetActive(true);
                 cam.GetComponent<TouchCameraController2>().enabled = false;
                 joystick.SetActive(false);
@@ -95,6 +132,19 @@ public class TouchMultiTarget : MonoBehaviour
                 }
                 break;
             case "qingtongqi": //青铜器旋转
+                Dedtroyobj();
+                YuzhiiX(1);
+                text1.text = "";
+                text1.text = "<b>颂壶</b>为西周晚期青铜礼器，距今近三千年，是青铜范铸工艺与礼乐文明的代表。器形庄重，饰龙纹、环带纹，颈部兽首衔环，内壁铸有 151 字铭文，记载册命礼制。此复刻品遵循古法青铜冶炼铸造，再现西周高超冶金技艺与礼器神韵。";
+                uiimage[0].SetActive(true);
+                cam.GetComponent<TouchCameraController2>().enabled = false;
+                joystick.SetActive(false);
+                break;
+            case "bingqi"://兵器旋转
+                Dedtroyobj();
+                YuzhiiX(2);
+                text1.text = "";
+                text1.text = "<b>剑</b>，被誉为 “百兵之君”，是古代冶金技术的巅峰代表。早期为青铜铸造，吴越铸剑工艺冠绝天下，如越王勾践剑采用复合金属与防锈工艺。汉代后以百炼钢、淬火技术锻打钢铁剑，刃利坚韧。它既是实战兵器，也体现了古代冶炼技艺与礼乐文化的高度融合。";
                 uiimage[0].SetActive(true);
                 cam.GetComponent<TouchCameraController2>().enabled = false;
                 joystick.SetActive(false);
